fb4bf81fa52d9cb571d390a167a34f339490b13e
prasunmax <prasunmax@gmail.com>
2019-08-03 10:58:54 +0530

    updated includejs

--- js/include.js
+++ js/include.js
@@ -1,31 +1,37 @@
+$(function () {
+    includeHTML();
+
+});
+//add Rows based on previous columns
+function addRow(){
+    //Get the last row and then add the next row
+    var childLst = arguments.currentTarget.parentElement.children;
+    var thisNode = childLst[chldLst.length - 2];
+    var cloneNode = $(thisNode).clone(true, true);
+    $(cloneNode)
+}
+
+
 function includeHTML() {
     var z, i, elmnt, file, xhttp;
     /* Loop through a collection of all HTML elements: */
     z = document.getElementsByTagName("*");
     for (i = 0; i < z.length; i++) {
-      elmnt = z[i];
-      /*search for elements with a certain atrribute:*/
-      file = elmnt.getAttribute("w3-include-html");
-      if (file) {
-        /* Make an HTTP request using the attribute value as the file name: */
-        xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-          if (this.readyState == 4) {
-            if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-            if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-            /* Remove the attribute, and call this function once more: */
-            elmnt.removeAttribute("w3-include-html");
-            includeHTML();
-          }
+        elmnt = z[i];
+        /*search for elements with a certain atrribute:*/
+        file = elmnt.getAttribute("w3-include-html");
+        if (file) {
+            $.get(file, function (data) {
+                $(elem).html(data);
+                $(elem).removeAttribute("w3-include-html");
+            });
+            /* Exit the function: */
+            return;
         }
-        xhttp.open("GET", file, true);
-        xhttp.send();
-        /* Exit the function: */
-        return;
-      }
     }
-  }
-  function changeLocation(txt){
-    var loc = location.href.substring(0,location.href.lastIndexOf("/"));
-    location.href= loc+"/"+txt;
-  }
+}
+
+function changeLocation(txt) {
+    var loc = location.href.substring(0, location.href.lastIndexOf("/"));
+    location.href = loc + "/" + txt;
+}
